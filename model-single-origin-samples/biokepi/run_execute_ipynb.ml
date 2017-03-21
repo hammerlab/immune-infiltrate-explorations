8e062c347fd1759a6df879919a1cbcb5e9a87590
Eliza Chang <eliza.j.chang@gmail.com>
2017-03-21 12:58:21 -0400

    workflow that passes, but ipynb fails on incorrect data setup

--- model-single-origin-samples/biokepi/run_execute_ipynb.ml
+++ model-single-origin-samples/biokepi/run_execute_ipynb.ml
@@ -12,7 +12,9 @@ let host = Ketrew.EDSL.Host.parse "/tmp/KT-coclomachine/"
 let run_program = Machine.run_program biokepi_machine
 let (//) = Filename.concat
 let install_path = install_tools_path // "conda"
-let ii_pip_requirements_path = "/tmp/dummy-requirements.txt"
+let ii_conda_requirements_path = "/nfs-pool/ii/conda_requirements.txt"
+let pip_requirements_path = "/nfs-pool/ii/pip_requirements.txt"
+let reference_build = "b37"
 
 (* Create a new Conda env *)
 let conda_env =
@@ -20,7 +22,7 @@ let conda_env =
     setup_environment
       ~python_version:`Python3
       install_path
-      "ii"
+      "fullii"
   )
 let conda_ensure = Setup.Conda.(configured ~run_program ~host ~conda_env)
 let conda_init = Setup.Conda.init_env ~conda_env ()
@@ -28,7 +30,7 @@ let conda_init = Setup.Conda.init_env ~conda_env ()
 
 (*node that installs all immune infiltrate tools into ii environment, and
   creates empty file to check if installation exists*)
-let ii_witness = install_path // ".ii"
+let ii_witness = install_path // ".fullii"
 let ii_node =
   workflow_node (single_file ~host ii_witness)
     ~name:"Install immune infiltrate conda environment"
@@ -37,9 +39,10 @@ let ii_node =
     ]
     ~make:(run_program
              Program.(
-               conda_init &&
-               shf "pip install -r %s" ii_pip_requirements_path &&
-               shf "touch %s" ii_witness
+               conda_init
+               && shf "conda install --file=%s" ii_conda_requirements_path
+               && shf "pip install -r %s" pip_requirements_path
+               && shf "touch %s" ii_witness
              )
           )
 (* end of ii install *)
@@ -52,12 +55,15 @@ let submit_job
     workflow_node without_product
       ~name:("Execute jupyter notebook, " ^ (Filename.basename my_notebook))
       ~edges:[
+        depends_on (Biokepi.Tools.Pyensembl.cache_genome ~run_with:biokepi_machine ~reference_build);
         depends_on ii_node;
         depends_on conda_ensure;
       ]
       ~make:(run_program
                Program.(
-                 shf "jupyter nbconvert --to notebook %s --output %s --ExecutePreprocessor.timeout=%d" my_notebook my_notebook timeout
+                 conda_init (*initialize conda environment*)
+                 && Biokepi.Tools.Pyensembl.(set_cache_dir_command ~run_with:biokepi_machine)
+                 && shf "jupyter nbconvert --to notebook --execute %s --ExecutePreprocessor.timeout=%d" my_notebook timeout
                )
             )
   in
@@ -65,7 +71,7 @@ let submit_job
 
 (*Command line options*)
 let my_notebook =
-  let doc = "Jupyter notebook containing data-loading and trains a stan model" in
+  let doc = "Path to Jupyter notebook you want to execute" in
   Arg.(required & pos 0 (some string) None & info [] ~docv:"JUPYTER_IPYNB" ~doc)
 
 let timeout =
@@ -73,7 +79,7 @@ let timeout =
   Arg.(required & pos 1 (some int) None & info [] ~docv:"TIMEOUT_SECS" ~doc)
 
 let cmd =
-  let doc = "Execute jupyter notebook in-place." in
+  let doc = "Execute jupyter notebook. Saves to {mynotebook}.nbconvert.ipynb" in
   let version = "0.0.0" in
   let man = [
     `S "Description";
@@ -82,7 +88,7 @@ let cmd =
   Term.(const
     submit_job
         $ my_notebook
-        $timeout
+        $ timeout
        ),
   Term.(info "run_nbconvert" ~version ~doc ~man)
 
